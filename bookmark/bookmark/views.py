11c4e8ba6c30e13ccfdb4e21283e49ce20969a4c
2hongbi <foxgirin950@gmail.com>
2022-12-30 20:54:18 +0900

    북마크 완성

--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
 
@@ -8,6 +9,7 @@ from .models import Bookmark
 
 class BookmarkListView(ListView):
     model = Bookmark
+    paginate_by = 6
 
 
 class BookmarkCreateView(CreateView):
@@ -15,3 +17,18 @@ class BookmarkCreateView(CreateView):
     fields = ['site_name', 'url']  # 어떤 필드들을 입력받을 것인지
     success_url = reverse_lazy('list')  # 글쓰기를 완료하고 이동할 페이지
     template_name_suffix = '_create'  # 사용할 템플릿의 접미사만 변경하는 설정값
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
+
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
