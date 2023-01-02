1bc7b2c1895f57b8a7d073688611f8a6833b9c66
2hongbi <foxgirin950@gmail.com>
2023-01-03 00:50:31 +0900

    add gitignore file

--- dstagram/config/settings.py
+++ dstagram/config/settings.py
@@ -36,13 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photo',
     'accounts',
     'disqus',
     'django.contrib.sites',
+    'storages',
 ]
 
 MIDDLEWARE = [
