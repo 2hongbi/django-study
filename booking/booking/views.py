56401b2164fe45e5fe7ac7382b276b9983657bed
2hongbi <foxgirin950@gmail.com>
2023-01-10 19:21:47 +0900

    API 인증 토큰 구성하기

--- booking/booking/views.py
+++ booking/booking/views.py
@@ -3,6 +3,11 @@ from rest_framework import generics
 from .models import Booking
 from .serializers import BookingSerializer
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsOwnerOrReadOnly
+
 
 class BookingList(generics.ListCreateAPIView):
     queryset = Booking.objects.all()
@@ -10,5 +15,8 @@ class BookingList(generics.ListCreateAPIView):
 
 
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,IsOwnerOrReadOnly)
+
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
--- booking/config/settings.py
+++ booking/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'booking',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_yasg',
 ]
 
@@ -126,3 +127,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+      'SECURITY_DEFINITIONS': {
+         'DRF Token': {
+               'type': 'apiKey',
+               'name': 'Authorization',
+               'in': 'header'
+         }
+      },
+    "LOGIN_URL": "/admin/login/",
+    "LOGOUT_URL": "/admin/logout/"
+   }
